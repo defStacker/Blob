6461ddf88ac907d83c8858925f1e1a7294b20030
defStacker <yachayx@gmail.com>
2017-02-07 16:42:03 -0500

    Update second.sh

--- second.sh
+++ second.sh
@@ -1,23 +1,12 @@
 #!/bin/bash
-##
-## Installs the pre-requisites for running edX on a single Ubuntu 12.04
-## instance.  This script is provided as a convenience and any of these
-## steps could be executed manually.
-##
-## Note that this script requires that you have the ability to run
-## commands as root via sudo.  Caveat Emptor!
-##
 
-##
-## Sanity check
-##
 if [[ `lsb_release -rs` != "16.04" ]]; then
    echo "This script is only known to work on Ubuntu 16.04, exiting...";
    exit;
 fi
 
 ##
-## Set ppa repository source for gcc/g++ 4.8 in order to install insights properly
+## Set ppa repository source 
 ##
 sudo apt-get install -y python-software-properties
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
@@ -36,10 +25,7 @@ sudo pip install --upgrade pip==8.1.2
 sudo pip install --upgrade setuptools==24.0.3
 sudo -H pip install --upgrade virtualenv==15.0.2
 
-##
-## Overridable version variables in the playbooks. Each can be overridden
-## individually, or with $OPENEDX_RELEASE.
-##
+
 export OPENEDX_RELEASE="open-release/ficus.master"
 
  
@@ -65,10 +51,6 @@ else
 fi
 
 
-
-##
-## Clone the configuration repository and run Ansible
-##
 cd /var/tmp
 git clone https://github.com/defStacker/configuration
 cd configuration
@@ -82,7 +64,6 @@ cd /var/tmp/configuration
 sudo -H pip install -r requirements.txt
 
 ##
-## Run the edx_sandbox.yml playbook in the configuration/playbooks directory
 ##
 cd /var/tmp/configuration/playbooks 
 
